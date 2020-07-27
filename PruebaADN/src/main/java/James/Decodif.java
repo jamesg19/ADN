15561da6e0c76b1bdb7639e3b5dd12954ead8493
jamesg19 <48737452+jamesg19@users.noreply.github.com>
2020-07-27 12:54:33 -0400

    branch

--- PruebaADN/src/main/java/James/Decodif.java
+++ PruebaADN/src/main/java/James/Decodif.java
@@ -14,9 +14,14 @@ import java.util.Scanner;
 public class Decodif {
     String SecuenciaADN1;
     String SecuenciaADN2;
-    int ADN1;
-    int ADN2;
-    
+    int LongitudADN1;
+    int LongitudADN2;
+    String Resultado;
+    String[] pal=new String[15];
+    String SubCadenaADN1;
+    String SubCadenaADN2;
+    Boolean ADN1;
+    Boolean ADN2;
     Scanner entrada=new Scanner(System.in);
     
     public void PedirSecuencia1(){
@@ -29,15 +34,47 @@ public class Decodif {
     public void PedirSecuencia2(){
         System.out.println("Ingrese la secuencia 2 de ADN:");
         SecuenciaADN2=entrada.nextLine();
-        DeterminarLongitud();
+        DeterminarLongitudADN();
     }
     
     
-    public void DeterminarLongitud(){
-        ADN1=SecuenciaADN1.length();
-        ADN2=SecuenciaADN2.length();
+    public void DeterminarLongitudADN (){
+        LongitudADN1=SecuenciaADN1.length();
+        LongitudADN2=SecuenciaADN2.length();
         
+            if(LongitudADN1==LongitudADN2){
+                DeterminarCadena();
+            }
+            else{
+                System.out.println("La cadena de ADN1 contiene: "+LongitudADN1+" digitos"+"\n"
+                +"La cadena de ADN2 contiene: "+LongitudADN2+" digitos"+"\n"
+                +"DEBEN DE TENER EL MISMO NUMERO DE DIGITOS");
+                PedirSecuencia1();
+                System.out.println("|||||||||||||||||");
+            }
         
+                
+    }
+    
+    public void DeterminarCadena(){
+        
+        for(int i=0; i<=LongitudADN1;i++){
+            
+           for (int j = 0; j <LongitudADN1; j++){
+             
+    
+            if(SecuenciaADN2.contains(SecuenciaADN1.substring(j, i))&&
+                    
+                    (SubCadenaADN1.length() < SecuenciaADN1.substring(i, j).length())){
+                SubCadenaADN1 = SecuenciaADN1.substring(i, j);
+                        
+            //System.out.println( SecuenciaADN1.substring(i, j) );
+            } else{
+                System.out.println("No se encuentran Secuencias");
+            } 
+           }  
+        }
+        System.out.println(SubCadenaADN1);
     }
     
 }
